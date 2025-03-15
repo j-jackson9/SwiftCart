52800b6c2928c984c00d4ea988ec17d7588b8b01
j-jackson9 <jordancjack.20@gmail.com>
2025-03-15 17:30:54 -0500

    added cart quantity and add to cart feature, added a cart page

--- index.js
+++ index.js
@@ -10,5 +10,20 @@ for (let button of addToCart) {
         let quantity = parseInt(quantityInput.value) || 1;
         totalCartItems += quantity;
         cart.innerHTML = totalCartItems;
+        alert(`Added ${quantity} item(s) to your cart!`);
     });
 }
+
+let quantityInputs = document.getElementsByClassName('quantity');
+for (let input of quantityInputs) {
+    input.addEventListener('input', () => {
+        // Get the value from the input field and ensure it's within the allowed range (1-4)
+        let value = parseInt(input.value);
+        if (value < 1) {
+            input.value = 1; // Reset to 1 if less than 1
+        } else if (value > 4) {
+            input.value = 4; // Limit to max 4
+        }
+    });
+}
+
