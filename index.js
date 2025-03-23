48c4c7456ada0958d4957509c5f39b55263e8638
j-jackson9 <jordancjack.20@gmail.com>
2025-03-22 20:57:31 -0500

    start over from square 1 on the add to cart feature

--- index.js
+++ index.js
@@ -1,53 +1,24 @@
-let addToCart = document.getElementsByClassName('add-to-cart');
-let cart = document.getElementsByClassName('cart-badge')[0];
-let totalCartItems = 0;
+let addToCartBtn = document.querySelectorAll('.add-to-cart');
+let itemCount = document.querySelector('.cart-badge');
+let cartTotal = parseInt(itemCount.innerHTML) || 0;
+let itemQuantity = document.querySelector('.quantity');
 
-cart.innerHTML = totalCartItems;
 
-for (let button of addToCart) {
-    button.addEventListener('click', () => {
-        let quantityInput = button.previousElementSibling.querySelector('.quantity'); 
-        let quantity = parseInt(quantityInput.value) || 1;
-        totalCartItems += quantity;
-        cart.innerHTML = totalCartItems;
-        alert(`Added ${quantity} item(s) to your cart!`);
 
-            let img = document.querySelector('.item img');
-            let name = document.querySelector('.name');
-            let price = document.querySelector('.price');
-        
-            let item = {
-                image: img.src,
-                name: name.innerText,
-                price: price.innerText
-            };
-        
-            localStorage.setItem('item', JSON.stringify(item));
-            let storedItem = JSON.parse(localStorage.getItem('item'));
-        } );
-};
 
-let quantityInputs = document.getElementsByClassName('quantity');
-for (let input of quantityInputs) {
-    input.addEventListener('input', () => {
-        // Get the value from the input field and ensure it's within the allowed range (1-4)
-        let value = parseInt(input.value);
-        if (value < 1) {
-            input.value = 1; // Reset to 1 if less than 1
-        } else if (value > 4) {
-            input.value = 4; // Limit to max 4
-        }
+
+
+
+
+
+addToCartBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        alert('Item(s) added to cart');
+         // Get the quantity from the specific input field next to the clicked button
+        let itemQuantity = btn.previousElementSibling.querySelector('.quantity');
+        let quantity = parseInt(itemQuantity.value) || 1;
+        cartTotal += quantity;
+        itemCount.innerHTML = cartTotal;
+        console.log(itemCount.innerHTML);
     });
-}
-
-
-function displayCart() {
-    let storedItem = JSON.parse(localStorage.getItem('item'));
-    let cartItem = document.createElement('div');
-    cartItem.innerHTML = `
-        <img src="${storedItem.image}" alt="item">
-        <p>${storedItem.name}</p>
-        <p>${storedItem.price}</p>
-    `;
-    document.getElementsByClassName('cart-items')[0].appendChild(cartItem);
-}
+});
